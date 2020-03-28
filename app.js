8481cab8cc9b58b1df4b6da73955d5ff98e4ef67
Daniel Nery <danielnso97@gmail.com>
2020-03-28 17:24:59 -0300

    Nem sei

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
-import Discord from "discord.js";
+import { Client, MessageAttachment } from "discord.js";
+import config from "./config.json";
 
-const client = new Discord.Client();
+const client = new Client();
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(`nice combat videos`, { type: "WATCHING" });
 });
 
-client.on("message", msg => {
-  console.log(`Got ${msg.content}`);
+client.on("message", async msg => {
+  if (msg.author.bot || !msg.content.startsWith(config.prefix)) {
+    return;
+  }
 
-  if (msg.content === "-renzo") {
-    const attachment = new Discord.MessageAttachment(
+  const args = msg.content
+    .slice(config.prefix.length)
+    .trim()
+    .split(" ");
+  const command = args.shift().toLowerCase();
+
+  if (command === "renzo") {
+    const attachment = new MessageAttachment(
       `https://static.thunderatz.org/thunderbot/renzo/renzo${Math.floor(Math.random() * 9) + 1}.png`
     );
     msg.channel.send(attachment);
+  }
+
+  if (command === "ping") {
+    const m = await msg.channel.send("Ping?");
+    m.edit(`Pong! ${m.createdTimestamp - msg.createdTimestamp}ms.`);
   }
 });
 
