7cb51defb8620d953c7774816817ce6792b8e2d8
Daniel Nery <danielnso97@gmail.com>
2020-03-29 19:35:41 -0300

    Add dst command

---
+++ commands/dst.js
@@ -0,0 +1,15 @@
+export const run = async (client, msg, args) => {};
+
+export const conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: "User",
+};
+
+export const help = {
+  name: "dst",
+  category: "X",
+  description: "Controle pro servidor de Don't Starve Together.",
+  usage: "dst [...]",
+};
