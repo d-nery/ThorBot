2dab9e8337f2c943ec4a1d3f9253902a88909a13
Daniel Nery <danielnso97@gmail.com>
2020-04-01 20:11:29 -0300

    Add seed to minecraft command

--- commands/minecraft.js
+++ commands/minecraft.js
@@ -1,17 +1,20 @@
 export const run = async (client, msg, args) => {
-  msg.channel.send("ThunderServidor de Minecraft: `35.198.60.125:25565`");
+  msg.channel.send(`**ThunderServidor de Minecraft**
+Endereço:  \`35.198.60.125:25565\`
+Seed: \`[4230481854676846495]\`
+`);
 };
 
 export const conf = {
   enabled: true,
   guildOnly: false,
   aliases: ["mine"],
-  permLevel: "User",
+  permLevel: "User"
 };
 
 export const help = {
   name: "minecraft",
   category: "Jogos",
   description: "Mostra o endereço do ThunderServidor de Minecraft.",
-  usage: "minecraft",
+  usage: "minecraft"
 };
